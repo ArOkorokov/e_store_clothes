21e7cf5f6056cf2c2723b2011d8498c46470e86f
KichiroMotidzuki <ar.okorokov@gmail.com>
2023-09-30 06:55:16 +0400

    almost there

--- js/swiperComments.js
+++ js/swiperComments.js
@@ -3,9 +3,34 @@
 
 
 
-const swiper1 = new Swiper('.swiper-comments', {
+const swiperComments = new Swiper('.swiper__comments', {
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 3,
-    
+    slidesPerView: 3.2,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      480: {
+        slidesPerView: 1.2,
+      },
+      545: {
+        slidesPerView: 1.4,
+      },
+      767.8: {
+        slidesPerView: 2,
+      },
+      991.8: {
+        slidesPerView: 2.5,
+      },
+      1280: {
+        slidesPerView: 3.2,
+
+      }
+    }
   });
